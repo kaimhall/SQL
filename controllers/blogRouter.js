9c8d52ccb0b0747cf1ddf7130783956216d6f4d1
kaimhall <kaimhall@gmail.com>
2022-06-21 19:38:36 +0300

    13.10. added join tables

--- controllers/blogRouter.js
+++ controllers/blogRouter.js
@@ -1,46 +1,64 @@
 const router = require('express').Router()
-const { Blog, User } = require('../models')
+const jwt = require('jsonwebtoken')
+const {Blog, User} = require('../models')
+
+const {SECRET} = require('../utils/config')
 
 const blogFinder = async (req) => {
   req.blog = await Blog.findByPk(req.params.id)
 }
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch {
+      res.status(401).json({error: 'token invalid'})
+    }
+  } else {
+    res.status(401).json({error: 'token missing'})
+  }
+  next()
+}
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
+  const blogs = await Blog.findAll({
+    attributes: {exclude: ['userId']},
+    include: {
+      model: User,
+      attributes: ['username'],
+    },
+  })
   res.json(blogs)
-}
-)
+})
 
-router.post('/', async (req, res) => {
-  const user = await User.findOne()
-  const blog = await Blog.create({ ...req.body, userId: user.id })
+router.post('/', tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  const blog = await Blog.create({...req.body, userId: user.id})
   res.json(blog)
-}
-)
+})
 
 router.get('/:id', blogFinder, async (req, res) => {
   if (req.note) {
     res.json(req.note)
   } else {
-    res.status(404).send({ error: 'blog not found' })
+    res.status(404).send({error: 'blog not found'})
   }
 })
 
 router.delete('/:id', blogFinder, async (req, res) => {
   if (req.note) {
     await req.note.destroy()
-  }
-
-  else {
-    res.status(204).send({ error: 'blog not found' })
+  } else {
+    res.status(204).send({error: 'blog not found'})
   }
 })
 
 // eslint-disable-next-line no-unused-vars
 router.delete('/', async (req, res) => {
   await Blog.destroy({
-    truncate: true
+    truncate: true,
   })
   res.json('table wiped')
 })
@@ -51,7 +69,7 @@ router.put('/:id', blogFinder, async (req, res) => {
     await req.blog.save()
     res.json(req.blog)
   } else {
-    res.status(404).send({ error: 'blog not found' })
+    res.status(404).send({error: 'blog not found'})
   }
 })
 
--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 
 const User = require('../models/user')
+const Blog = require('../models/blogs')
 
 /*
 const userFinder = async (req) => {
@@ -13,7 +14,13 @@ const userFinder = async (req) => {
 */
 
 router.get('/', async (req, res) => {
-  const users = await User.findAll()
+  const users = await User.findAll({
+    include: {
+      model: Blog,
+      attributes: {exclude: ['userId']},
+    },
+  })
+
   res.json(users)
 })
 
@@ -25,8 +32,8 @@ router.post('/', async (req, res) => {
 router.put('/:username', async (req, res) => {
   const usr = await User.findOne({
     where: {
-      username: req.params.username
-    }
+      username: req.params.username,
+    },
   })
   //console.log(JSON.stringify(usr, null, 2))
   usr.username = req.body.username
@@ -39,22 +46,16 @@ router.delete('/:id', async (req, res) => {
   if (usr) {
     await usr.destroy()
     res.json(usr, 'deleted!')
-  }
-  else {
-    res.status(204).send({ error: 'blog not found' })
+  } else {
+    res.status(204).send({error: 'blog not found'})
   }
 })
 
 router.delete('/', async (req, res) => {
   await User.destroy({
-    truncate: true
+    truncate: true,
   })
   res.json('table wiped')
 })
-
-
-
-
-
 
 module.exports = router
