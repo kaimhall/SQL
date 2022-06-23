f48b00568e8e8e357e4230b88d01f27b6cbce303
kaimhall <kaimhall@gmail.com>
2022-06-23 14:50:09 +0300

    13.18 added year field to users

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -3,12 +3,12 @@
 const errorHandler = (error, request, response, next) => {
   //remember error.name && 1st line of error msg!
   if (error.name === 'SequelizeValidationError') {
-    return response.status(400).send({error: 'username must be an email.'})
-  }
-  if (error.name === 'TypeError') {
     return response
       .status(400)
-      .send({error: 'parameter is null. please input valid parameters.'})
+      .send({error: error.errors.map((e) => e.message)})
+  }
+  if (error.name === 'TypeError') {
+    return response.status(400).send({error: error})
   }
 
   next(error)
